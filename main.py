1f2c3f2a5481cbf0cbbba93a8512327a6f96d3da
neiro2 <neirouniversiti.@gmail.com>
2024-12-18 15:29:18 +0300

    add new const

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+from turtledemo.nim import SCREENWIDTH
+
 import  pygame
+from pygame.examples.scrap_clipboard import screen
 
 pygame.init()
+
+SCREEN_WIDTH = 800 # ширина экрана
+SCREEN_HEIGHT = 600 # длина экрана
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT )) # установили экран с размерами
+
+pygame.display.set_caption('Игра Тир') # название игры
+
+icon = pygame.image.load
+
+
+
 
 running = True
 while running:
